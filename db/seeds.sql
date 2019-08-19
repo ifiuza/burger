140a2c6b47043e828089ea0dffeccffd3300c5b8
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-08-19 17:13:37 -0600

    db setup done

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger with bacon');
+INSERT INTO burgers (burger_name) VALUES ('Double-Cheeseburger with bacon');
+INSERT INTO burgers (burger_name) VALUES ('Triple-Cheeseburger with bacon');
+
