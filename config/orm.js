e8fceeed059871651d3a2c8432c2bd8c0e181cc4
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-08-19 18:37:54 -0600

    controller setup done

---
+++ config/orm.js
@@ -0,0 +1,42 @@
+var connection = require("./connection.js");
+
+var orm = {
+	selectAll: () => {
+		var queryString = "SELECT * FROM burgers_db.burgers";
+		console.log(queryString);
+
+		connection.query(queryString, (err, res) => {
+			console.log(res);
+		});
+	},
+
+	insertOne: () => {
+		var queryString = `INSERT INTO burgers_db.burgers(burger_name)
+		VALUES ?`;
+		var value;
+
+		console.log(queryString);
+
+		connection.query(queryString, value, (err, res) => {
+			if (err) throw err;
+
+			console.log(res);
+		});
+    },
+    
+    updateOne: () => {
+		var queryString = `UPDATE INTO burgers_db.burgers(burger_name)
+		VALUES ?`;
+		var value;
+
+		console.log(queryString);
+
+		connection.query(queryString, value, (err, res) => {
+			if (err) throw err;
+
+			console.log(res);
+		});
+	},
+};
+
+module.exports = orm;
